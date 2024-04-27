a782ee108b697998d18c8b89b0f661dac51bea9f
zeyad al-houssainy <zeyad-houssainy@hotmail.com>
2024-04-27 22:34:16 +0300

    Update3

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,25 +3,25 @@
 const request = require('request');
 
 const req = (arr, i) => {
-	if (i === arr.length) return;
-	request(arr[i], (err, response, body) => {
-		if (err) {
-			throw err;
-		} else {
-			console.log(JSON.parse(body).name);
-			req(arr, i + 1);
-		}
-	});
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
 };
 
 request(
 	`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
 	(err, response, body) => {
-		if (err) {
-			throw err;
-		} else {
-			const chars = JSON.parse(body).characters;
-			req(chars, 0);
-		}
+	  if (err) {
+	    throw err;
+	  } else {
+	    const chars = JSON.parse(body).characters;
+	    req(chars, 0);
+	  }
 	}
 );
